d2544958c84ac8d2e67f29b89014bc89c6b66998
adamabramowitz0 <ahabramowitz@gmail.com>
2024-03-03 15:39:42 -0500

    CLEANED UP ish

--- Data_AnalysisExperiment/venv/MessyPy.py
+++ Data_AnalysisExperiment/venv/MessyPy.py
@@ -14,23 +14,13 @@ lock = threading.Lock()
 ser = serial.Serial('/dev/tty.usbserial-B001QT7U', 57600, timeout = 500)
 
 
-#USER SETS THE FOLLOWING
-#List of sensors
-sensors = ["Thing 1","Thing 2","Thing 3","Thing 4"]
-frequencyHz = [100,100,100,100]
-largestFreq = 100
-
-#############
-
-    
-#GLOBAL VARIABLES THAT PROBABLY SHOULDNT EXIST
-mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#SETUP
+SENSORS = ["Thing 1","Thing 2","Thing 3","Thing 4"]
+FREQUENCYHZ = [100,100,100,100]
+LARGESTFREQ = 100
+TIMETORUN = 20 #Seconds
+MYSOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dictionary = {}
-
-
-#line2, = ax.plot([],[])
-#line3, = ax.plot([],[]) 
-#############
 
 #Decimal Setup
 getcontext().prec = 6
@@ -40,12 +30,14 @@ getcontext().prec = 6
 
 def main():
     fillDictionaryWithZeros()
-    
     freqThread = threading.Thread(target= getAndShowData)
-    
     freqThread.start()
     initilizeGraph()
     freqThread.join()
+
+
+
+
 
 
 def updateGraph(t, ax,plots):
@@ -54,88 +46,93 @@ def updateGraph(t, ax,plots):
 
     ax.set_xlim(timeLowerBound, timeUpperbound, auto=False)
 
-    for plot, sensor in zip(plots, sensors):
+    for plot, sensor in zip(plots, SENSORS
+
+):
         #with lock:
        plot.set_data(dictionary["time"], dictionary[sensor])
             #z[q].set_data([0,1,2],[0,1,2])
      
     return plots
 
-
-def initilizeGraph():
+def initilizeGraph(dataDict):
     plots = []
     fig,ax = plt.subplots()
     ax.set_ylim(0,370)
     ax.set_xlim(0,1)
-    for i in range(0,len(sensors)):
+    for i in range(0,len(SENSORS)):
         plots.append(ax.plot([],[], linewidth = 1, alpha=.8)[0])
-        plots[i].set_label(sensors[i])
+        plots[i].set_label(SENSORS[i])
+
     ax.legend()
     
     print("Starting graph animation.")
-    garbageCollectionStopper = FuncAnimation(fig = fig,func = updateGraph, fargs=(ax,plots), frames = 1000000, interval =200, blit=False)
+    garbageCollectionStopper = FuncAnimation(fig = fig,func = updateGraph, fargs=(ax,plots, dataDict), frames = 1000000, interval =200, blit=False)
     plt.show()
 
 def fillDictionaryWithZeros():
         global dictionary
-        for i in sensors:
+        for i in SENSORS:
             dictionary[i] = [0]
         dictionary["time"] = [0]
 
 def fillDict(iteration,z,data):
     with lock:
-        dictionary[sensors[z]].append(data)
-        dictionary["time"].append(Decimal(iteration)/Decimal(largestFreq))
-        for i in sensors:
-            if(i != sensors[z]):
+        dictionary[SENSORS[z]].append(data)
+        dictionary["time"].append(Decimal(iteration)/Decimal(LARGESTFREQ))
+        for i in SENSORS:
+            if(i != SENSORS[z]):
                 dictionary[i].append(dictionary[i][-1:][0])
 
 
-def tryToEstablishSocket(runnable):
-    try:
 
-        #mySocket.setConnectionTimeout(500000)
-        mySocket.connect(("localhost", 8084))
-        
-        mySocket.sendall(bytes([len(frequencyHz)]))
-
-        for i in range(0,len(arrayOfBytes)):
-            mySocket.sendall(bytes([len(frequencyHz[i])]))
-            mySocket.sendall(bytes(frequencyHz[i],'utf-8'))
-            mySocket.sendall(bytes([int(frequencyHz[i]/256)]))
-            mySocket.sendall(bytes([frequencyHz[i]%256]))
-        return True
-    except:
-        print("Cannot connect to server!")
-        return False
-
-
-def sendData(index, value, runnable): 
-    if(runnable):
-
-        mySocket.sendall(bytes([len(frequencyHz[index])]))
-        mySocket.sendall(bytes(frequencyHz[i],'utf-8'))
-        mySocket.sendall(bytes([int(value)]))
 
 
 def getAndShowData():
-    runnable = True
     iteration = 0
-    while iteration < 20000:
-        
-        for index in range(0,len(frequencyHz)):
-
-            if((iteration)%(float(largestFreq/frequencyHz[index])) == 0):
-                if (iteration == 0) and (index == 0):
-                    runnable = tryToEstablishSocket(runnable)
-                #value = (iteration/(index+3)-random.randint(0,10))%150
-                value =  int.from_bytes(ser.read(8), "big")
-                sendData(index, value, runnable) #data curently random
+    while iteration < TIMETORUN * LARGESTFREQ:
+        for index in range(0,len(FREQUENCYHZ)):
+            if((iteration)%(float(LARGESTFREQ/FREQUENCYHZ[index])) == 0): #checks with frequency
+                value =  int.from_bytes(ser.read(4), "big")
                 fillDict(iteration,index,value)
 
         iteration+=1
-        time.sleep(float(1/largestFreq))    #adjust for time of for loop    
+        time.sleep(float(1/LARGESTFREQ))    #adjust for time of for loop    
     df = pd.DataFrame.from_dict(dictionary)
     df.to_excel("ModularTest.xlsx")
+    df.to_csv("CSVForRobi.csv")
+
+
 
 main()
+
+
+
+
+# DO NOT DELETE BELOW!
+
+# def tryToEstablishSocket(runnable):
+#     try:
+
+#         #MYSOCKET.setConnectionTimeout(500000)
+#         MYSOCKET.connect(("localhost", 8084))
+        
+#         MYSOCKET.sendall(bytes([len(FREQUENCYHZ)]))
+
+#         for i in range(0,len(SENSORS)):
+#             MYSOCKET.sendall(bytes([len(FREQUENCYHZ[i])]))
+#             MYSOCKET.sendall(bytes(FREQUENCYHZ[i],'utf-8'))
+#             MYSOCKET.sendall(bytes([int(FREQUENCYHZ[i]/256)]))
+#             MYSOCKET.sendall(bytes([FREQUENCYHZ[i]%256]))
+#         return True
+#     except:
+#         print("Cannot connect to server!")
+#         return False
+
+
+# def sendData(index, value, runnable): 
+#     if(runnable):
+
+#         MYSOCKET.sendall(bytes([len(FREQUENCYHZ[index])]))
+#         MYSOCKET.sendall(bytes(FREQUENCYHZ[index],'utf-8'))
+#         MYSOCKET.sendall(bytes([int(value)]))
