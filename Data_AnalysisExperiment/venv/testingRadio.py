b4ed828265869d82542a5aae66db3f8eb1520c19
Susan <105997753+cvesmarony@users.noreply.github.com>
2024-03-03 16:05:18 -0500

    Add files via upload

--- Data_AnalysisExperiment/venv/testingRadio.py
+++ Data_AnalysisExperiment/venv/testingRadio.py
@@ -1,10 +1,11 @@
 import time
 import serial
 
-ser = serial.Serial('/dev/tty.usbserial-B001QT7U', 57600, timeout = 500)
+ser = serial.Serial('COM3', 57600, timeout = 500)
 
 
 print(ser.name)
 while True:
         
         print(int.from_bytes(ser.read(4), "big"))
+        time.sleep(1)
